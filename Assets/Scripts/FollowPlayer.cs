2659a5765893c85c79688851e53cfd7811f82526
FlakySeaSalt <avinashmethil31@gmail.com>
2026-05-16 23:18:29 +0530

    add move cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,17 +6,49 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField]
     private GameObject playerGameObject;
     [SerializeField]
-    private float desiredTime =3f;
+    private float desiredTimeX =3f;
     [SerializeField]
     private float YDeadZone =1.5f;
-    private float elapsedTime = 0f;
+    [SerializeField]
+    private float desiredTimeY =3f;
+    enum state
+    {
+        TrackingX,
+        TrackingXY
+    }
+    state currentState = state.TrackingX;
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        float percentageComplete = elapsedTime / desiredTime;
-        transform.position = new Vector3(Vector3.Lerp(transform.position, playerGameObject.transform.position,Mathf.SmoothStep(0,1, percentageComplete)).x,
-                                         transform.position.y,
-                                         transform.position.z);
-
+        switch(currentState)
+        {
+            case state.TrackingX:
+                TrackXZone();
+                break;
+            case state.TrackingXY:
+                TrackXYZone();
+                break;
+        }    
     }
+
+    void TrackXZone()
+    {
+      
+        if(Mathf.Abs(transform.position.y - playerGameObject.transform.position.y) > YDeadZone)
+        {
+            currentState = state.TrackingXY;
+        }
+
+        transform.position = new Vector3(Mathf.MoveTowards(transform.position.x,playerGameObject.transform.position.x,desiredTimeX),transform.position.y,transform.position.z);
+    }
+
+    void TrackXYZone()
+    {
+        if(transform.position.y == playerGameObject.transform.position.y)
+        {
+            currentState = state.TrackingX;
+        }
+        transform.position = new Vector3(Mathf.MoveTowards(transform.position.x,playerGameObject.transform.position.x,desiredTimeX),
+                                         Mathf.MoveTowards(transform.position.y,playerGameObject.transform.position.y,desiredTimeY),
+                                         transform.position.z);
+    }    
 }
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    float Movetimer = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
